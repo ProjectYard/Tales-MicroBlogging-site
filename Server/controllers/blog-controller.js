410a2c30f2ef71b9b05fb098f200335158336d07
Ashish Sahu <ashishsahu10428@gmail.com>
2023-03-24 00:38:45 +0530

    More method done
    - PUT method
    - DELETE method

--- Server/controllers/blog-controller.js
+++ Server/controllers/blog-controller.js
@@ -1,30 +1,77 @@
-import Blog from '../models/Blog';
+import Blog from "../models/Blog";
 
-export const getAllBlogs = async (req,res,next)=>{
-    let blogs;
-    try {
-        blogs = await Blog.find();
-    } catch (err) {
-        return console.log(err);
-    }
-    if(!blogs){
-        return res.status(404).json({message:"No blogs found"});
-    }
-    return res.status(200).json({blogs})
-}
+export const getAllBlogs = async (req, res, next) => {
+  let blogs;
+  try {
+    blogs = await Blog.find();
+  } catch (err) {
+    return console.log(err);
+  }
+  if (!blogs) {
+    return res.status(404).json({ message: "No blogs found" });
+  }
+  return res.status(200).json({ blogs });
+};
 
-export const postBlog = async (req,res,next) =>{
-    const {title,description,image,user} = req.body;
-    const blog = new Blog({
-        title,
-        description,
-        image,
-        user
+export const postBlog = async (req, res, next) => {
+  const { title, description, image, user } = req.body;
+  const blog = new Blog({
+    title,
+    description,
+    image,
+    user,
+  });
+  try {
+    await blog.save();
+  } catch (err) {
+    return console.log(err);
+  }
+  return res.status(201).json({ blog });
+};
+
+export const editBlog = async (req, res, next) => {
+  const blogId = req.params.id;
+  const { title, description } = req.body;
+  let blog;
+  try {
+    blog = await Blog.findByIdAndUpdate(blogId, {
+      title,
+      description,
     });
-    try {
-      await blog.save();
-    } catch (err) {
-      return console.log(err);
-    }
-    return res.status(201).json({ blog });
-}
+  } catch (error) {
+    return console.log(error);
+  }
+  if (!blog) {
+    return res.status(500).json({ message: "Unable to update the blog" });
+  }
+  return res.status(200).json({ blog });
+};
+
+export const getBlogById = async (req, res, next) => {
+  const blogId = req.params.id;
+  let blog;
+  try {
+    blog = await Blog.findById(blogId);
+  } catch (error) {
+    return console.log(error);
+  }
+  if (!blog) {
+    return res.status(500).json({ message: "Unable to find the blog" });
+  }
+  return res.status(200).json({ blog });
+};
+
+export const delBlog = async (req, res, next) => {
+  const blogId = req.params.id;
+  let blog;
+  try {
+    blog = await Blog.findByIdAndDelete(blogId);
+  } catch (err) {
+    return console.log(err);
+  }
+  if (!blogId) {
+    return res.status(500).json({ message: "Data not found" });
+  }
+
+  return res.status(200).json({ message: blogId + " Deleted" });
+};
--- Server/routes/blog-routes.js
+++ Server/routes/blog-routes.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { getAllBlogs, postBlog } from "../controllers/blog-controller";
+import {
+  getAllBlogs,
+  postBlog,
+  editBlog,
+  getBlogById,
+  delBlog
+} from "../controllers/blog-controller";
 
 const blogrouter = express.Router();
 
 blogrouter.get("/", getAllBlogs);
 blogrouter.post("/add", postBlog);
-
+blogrouter.put("/edit/:id", editBlog);
+blogrouter.get("/:id", getBlogById);
+blogrouter.delete("/:id",delBlog)
 export default blogrouter;
